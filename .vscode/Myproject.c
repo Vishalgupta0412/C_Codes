fd73b094543d57de4002d4651d51be41949fb100
omtiwari12 <om.tiwari@seekware.in>
2023-10-04 12:51:01 +0530

    removed space

--- .vscode/Myproject.c
+++ .vscode/Myproject.c
@@ -15,7 +15,6 @@ int main()
     fptr = fopen("/home/dell/Desktop/Employeedata.txt", "a");
     for (int i = 0; i < 5; i++)
     {
-        
         fflush(stdin);
         printf("enter name:\n");
         gets(emp[i].name);
